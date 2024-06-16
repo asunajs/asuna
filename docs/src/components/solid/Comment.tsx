179a9e64b965a135d43f9803c82beeb9fa23c550
catlair <i@thefish.icu>
2024-06-16 12:46:31 +0800

    docs(comments): 切换仓库

--- docs/src/components/solid/Comment.tsx
+++ docs/src/components/solid/Comment.tsx
@@ -1,64 +1,65 @@
-import Giscus from '@giscus/solid';
-import { createEffect, createSignal } from 'solid-js';
+import Giscus from '@giscus/solid'
+import { createEffect, createSignal } from 'solid-js'
 
-const id = 'inject-comments';
+const id = 'inject-comments'
 
 function getCurrentTheme() {
-  const theme =
-    document.firstElementChild &&
-    document.firstElementChild.getAttribute('data-theme');
-  return theme || 'light';
+  const theme = document.firstElementChild
+    && document.firstElementChild.getAttribute('data-theme')
+  return theme || 'light'
 }
 
 const Comments = () => {
-  const [mounted, setMounted] = createSignal(false);
-  const [theme, setTheme] = createSignal('light');
+  const [mounted, setMounted] = createSignal(false)
+  const [theme, setTheme] = createSignal('light')
 
   createEffect(() => {
-    const theme = getCurrentTheme();
-    setTheme(theme);
+    const theme = getCurrentTheme()
+    setTheme(theme)
     // 监听主题变化
     const observer = new MutationObserver(() => {
-      setTheme(getCurrentTheme());
-    });
+      setTheme(getCurrentTheme())
+    })
     observer.observe(document.documentElement, {
       attributes: true,
       attributeFilter: ['data-theme'],
-    });
+    })
 
     // 取消监听
     return () => {
-      observer.disconnect();
-    };
-  }, []);
+      observer.disconnect()
+    }
+  }, [])
 
   createEffect(() => {
-    setMounted(true);
-  }, []);
+    setMounted(true)
+  }, [])
 
   return (
-    <div id={id} class="w-full">
-      {mounted() ? (
-        <div class="comments-container">
-          <Giscus
-            id={id}
-            repo="asunajs/asuna"
-            repoId="R_kgDOL2pMgQ"
-            category="Announcements"
-            categoryId="DIC_kwDOL2pMgc4CfHTT"
-            mapping="pathname"
-            strict="0"
-            reactions-enabled="1"
-            emit-metadata="0"
-            input-position="top"
-            theme={theme()}
-            lang="zh-CN"
-            loading="lazy"
-          />
-        </div>
-      ) : null}
+    <div id={id} class='w-full'>
+      {mounted()
+        ? (
+          <div class='comments-container'>
+            <Giscus
+              id={id}
+              repo='asunajs/docs'
+              repoId='R_kgDOL2pMgQ'
+              category='Announcements'
+              categoryId='DIC_kwDOL2pMgc4CfHTT'
+              mapping='pathname'
+              strict='0'
+              reactions-enabled='1'
+              emit-metadata='0'
+              input-position='top'
+              theme={theme()}
+              lang='zh-CN'
+              loading='lazy'
+            />
+          </div>
+        )
+        : null}
     </div>
-  );
-};
+  )
+}
 
-export default Comments;
+export default Comments
--- scripts/hidden_clean.mjs
+++ scripts/hidden_clean.mjs
@@ -18,6 +18,8 @@ function encrypt(text, password, iv) {
 }
 
 function main() {
+  if (!checkGit()) return
+
   const password = process.env.GIT_HIDDEN_PASSWORD
   const iv = process.env.GIT_HIDDEN_IV
   if (!password || !iv) {
@@ -30,6 +32,30 @@ function main() {
 
 function addFileForGit(path) {
   spawnSync('git', ['add', path])
+}
+
+function checkGit() {
+  // 执行git status -s命令
+  const gitStatus = spawnSync('git', ['status', '-s'])
+
+  if (gitStatus.status !== 0) {
+    console.error('Git status failed')
+    return false
+  }
+
+  // 读取命令的标准输出并解码为字符串
+  const output = gitStatus.stdout.toString()
+
+  // 过滤出已添加的文件（以"A"或"M"开头的行，忽略空格后是" "或"M"的情况，表示文件已添加或修改后添加）
+  const addedFiles = output.split('\n')
+    .filter(line => /^[AM]\s/.test(line))
+    .map(line => line.trim().slice(3)) // 去除前面的标识和空格，获取文件路径
+
+  // 输出已添加的文件列表
+  console.log('Added files:')
+  addedFiles.forEach(file => console.log(file))
+
+  return addedFiles.some(file => file.includes('core/caiyun') && file.endsWith('.ts'))
 }
 
 /**
@@ -46,10 +72,14 @@ function _encrypt(rootPath, password, iv) {
       if (!existsSync(path)) return
 
       if (file.endsWith('.ts') || file.endsWith('.js')) {
+        const text = readFileSync(file, 'utf8')
+
+        if (text.startsWith("export const _hiddenText = '")) return
+
         console.log(`Encrypting ${path}`)
         writeFileSync(
           path,
-          `export const _hiddenText = '${encrypt(readFileSync(path, 'utf8'), password, iv)}'`,
+          `export const _hiddenText = '${encrypt(text, password, iv)}'`,
         )
         addFileForGit(path)
         // 判断是否是文件夹
